19db9508613571dc37c546f88efac8a2670f543f
William Okano <williamokano@gmail.com>
2016-11-06 03:03:15 -0200

    Added guzzle adapter

---
+++ src/Adapters/GuzzleAdapter.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Katapoka\Ahgora\Adapters;
+
+use Katapoka\Ahgora\IHttpClient;
+
+/**
+ * Guzzle IHttpClient version
+ */
+class GuzzleAdapter implements IHttpClient
+{
+
+    /**
+     * Make an http request to some URL with the given http method
+     *
+     * @param string $method
+     * @param string $url
+     * @param array  $data
+     * @param array  $config
+     *
+     * @return \Katapoka\Ahgora\HttpResponse
+     */
+    public function request($method, $url, $data = array(), array $config = array())
+    {
+        // TODO: Implement request() method.
+    }
+
+    /**
+     * Make a get request to an URL.
+     *
+     * @param string $url
+     * @param array  $data
+     * @param array  $config
+     *
+     * @return \Katapoka\Ahgora\HttpResponse
+     */
+    public function get($url, $data = array(), array $config = array())
+    {
+        // TODO: Implement get() method.
+    }
+
+    /**
+     * Make a post request to an URL.
+     *
+     * @param string $url
+     * @param array  $data
+     * @param array  $config
+     *
+     * @return \Katapoka\Ahgora\HttpResponse
+     */
+    public function post($url, $data = array(), array $config = array())
+    {
+        // TODO: Implement post() method.
+    }
+
+    /**
+     * Set a header to the request.
+     *
+     * @param string $header
+     * @param string $value
+     *
+     * @return \Katapoka\Ahgora\IHttpClient the instance of the class for method chaining
+     */
+    public function setHeader($header, $value)
+    {
+        // TODO: Implement setHeader() method.
+    }
+
+    /**
+     * Retrieves the value of a given header name.
+     *
+     * @param string $header
+     *
+     * @return string
+     */
+    public function getHeader($header)
+    {
+        // TODO: Implement getHeader() method.
+    }
+
+    /**
+     * Set a timeout to the connection.
+     *
+     * @param int $ttl
+     *
+     * @return \Katapoka\Ahgora\IHttpClient
+     */
+    public function setTimeout($ttl)
+    {
+        // TODO: Implement setTimeout() method.
+    }
+}
--- tests/ApiTest.php
+++ tests/ApiTest.php
@@ -1,7 +1,9 @@
 <?php
 namespace Katapoka\Tests\Ahgora;
 
+use Katapoka\Ahgora\Adapters\GuzzleAdapter;
 use Katapoka\Ahgora\Api;
+use Katapoka\Ahgora\IHttpClient;
 use PHPUnit_Framework_TestCase;
 
 /**
@@ -11,7 +13,10 @@ class ApiTest extends PHPUnit_Framework_TestCase
 {
     public function testConstructor()
     {
-        $api = new Api();
+        $guzzleAdapter = new GuzzleAdapter();
+        $this->assertInstanceOf(IHttpClient::class, $guzzleAdapter);
+
+        $api = new Api($guzzleAdapter);
         $this->assertInstanceOf(Api::class, $api);
     }
 }
