aab4a6bc6051b54fe75ed6e1f731ec3f0385be95
William Okano <williamokano@gmail.com>
2016-11-06 20:05:42 -0200

    Added basic methods

--- src/Api.php
+++ src/Api.php
@@ -2,16 +2,107 @@
 
 namespace Katapoka\Ahgora;
 
+use InvalidArgumentException;
+
 /**
  * Class responsible for getting the data from the Ahgora system.
  */
 class Api
 {
+    const AHGORA_BASE_URL = 'https://www.ahgora.com.br/';
+
     /** @var \Katapoka\Ahgora\IHttpClient */
     private $httpClient;
+    /** @var string */
+    private $password;
+    /** @var string */
+    private $companyId;
+    /** @var string */
+    private $username;
+    /** @var bool */
+    private $loggedIn = false;
 
+    /**
+     * Api constructor.
+     *
+     * @param IHttpClient $httpClient
+     */
     public function __construct(IHttpClient $httpClient)
     {
         $this->httpClient = $httpClient;
+    }
+
+    /**
+     * Set the company id of the ahgora system.
+     *
+     * @param string $companyId
+     *
+     * @return $this
+     */
+    public function setCompanyId($companyId)
+    {
+        $this->companyId = $companyId;
+
+        return $this;
+    }
+
+    /**
+     * Set the username of the employee, from the company set at the setCompanyId.
+     *
+     * @param string $username
+     *
+     * @return $this
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Set the password of the employee, from the company set at the setCompanyId.
+     *
+     * @param string $password
+     *
+     * @return $this
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Try to execute the login on the page.
+     * To execute some actions the user needs to be loggedin.
+     * After a successful login, the status loggedin is saved as true.
+     *
+     * @return bool Returns true if the login was successful and false otherwise
+     */
+    public function doLogin()
+    {
+        $this->setLoggedIn(true);
+
+        return true;
+    }
+
+    /**
+     * Safely set if the user is loggedin or not.
+     * Did a separate method do eventually trigger events, if necessary.
+     *
+     * @param bool $loggedIn
+     *
+     * @return $this
+     */
+    private function setLoggedIn($loggedIn = true)
+    {
+        if (!is_bool($loggedIn)) {
+            throw new InvalidArgumentException('LoggedIn parameter must be boolean');
+        }
+        $this->loggedIn = $loggedIn;
+
+        return $this;
     }
 }
